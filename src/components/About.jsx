f6acc3b7a973af9415e1cb0fdfa12d06d4b28c14
Shweta Srivastava <shwetasrivastava20sln@gmail.com>
2022-04-23 12:02:59 +0530

    about completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,24 @@ export default function About() {
   return (
     <div>
         <section id="about">
-            <div className="container">
+            <div className="container my-5 py-5">
                 <div className="row">
                     <div className="col-md-6">
-                        <img src="" alt="about image" />
+                        <img src="/images/About.jpg" alt="" 
+                        className='w-75 mt-5' />
+                    </div>
+                    <div className="col-md-6">
+                        <h3 className="fs-5">
+                            About Us
+                        </h3>
+                        <h1 className="display-6 mb-2">
+                            What <b>We</b> Do
+                        </h1>
+                        <hr className='w-50' />
+                        <p className="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptate ab nulla est accusantium consequatur voluptatem veniam atque voluptas. Sunt totam repudiandae, ipsa natus quaerat dolore, accusantium officia veritatis quibusdam distinctio necessitatibus. Sint facilis incidunt repudiandae natus libero dolorem tenetur ipsam a blanditiis quas. Velit, ipsum autem? Cupiditate reiciendis omnis harum perspiciatis ipsa ullam, consectetur minima dolorum veritatis? Quia quisquam debitis modi, fugit ab similique?</p>
+                        <button className="btn btn-primary rounded-pill mx-4 py-2 px-4">More Info</button>
+                        <button className="btn btn-outline-primary rounded-pill mx-4 py-2 px-4">Contact</button>
+
                     </div>
                 </div>
             </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,9 +4,20 @@ const Home = () => {
   return (
     <div>
         <section id="home">
-            <div className="container"></div>
-            <div className="row"></div>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-md-8 mt-5">
+                  <h1 className="display-4 fw-bolder mb-4 text-center text-white">feel the fresh business perspective</h1>
+                  <p className="lead text-center fs-4 mb-5 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, sunt!</p>
+                  <div className="buttons d-flex justify-content-center">
+                    <button className="btn btn-light me-4 rounded-pill px-4 py-2">apply here</button>
+                    <button className="btn btn-outline-light rounded-pill px-4 py-2">our services</button>
+                  </div>
+                </div>
+              </div>
+            </div>
         </section>
     </div>
   )
 }
+export default Home
