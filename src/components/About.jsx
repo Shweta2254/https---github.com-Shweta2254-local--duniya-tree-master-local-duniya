06fda7bd5e94218a6c3885a7df033d074a422b26
Shweta Srivastava <shwetasrivastava20sln@gmail.com>
2022-04-23 10:34:03 +0530

    About Started

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <section id="about">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src="" alt="about image" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
