ffd8517cec4d917419194b3fa765f12005f319eb
Shweta Srivastava <shwetasrivastava20sln@gmail.com>
2022-04-23 09:00:57 +0530

    navbar completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <section id="home">
+            <div className="container"></div>
+            <div className="row"></div>
+        </section>
+    </div>
+  )
+}
